faeda9db1015c6831b802369e85ab4ab7908d9e9
sreeshmashaji <sreeshma3023@gmail.com>
2024-01-06 10:22:04 +0530

    new

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 echo "running sh file😊"
- - name: Pull Image
+
       run: sudo docker pull -a ${{ secrets.DOCKERHUB_USERNAME }}/clockbox
      
-    - name: Deploy Image
       run: sudo docker run -dp 8080:8080 ${{ secrets.DOCKERHUB_USERNAME }}/clockbox:latest
