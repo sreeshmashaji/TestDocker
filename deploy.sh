3f0658e0f52e10f1a57731754b56298dbd6dd7cc
sreeshmashaji <sreeshma3023@gmail.com>
2024-01-06 11:18:54 +0530

    new

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Pull the latest Docker image from GitHub Container Registry
-docker pull ${secrets.DOCKERHUB_USERNAME}/clockbox:latest
+sudo docker pull ${secrets.DOCKERHUB_USERNAME}/clockbox:latest
 
 # Stop and remove the existing container
-docker stop clockbox-container || true
-docker rm clockbox-container || true
+sudo docker stop clockbox-container || true
+sudo docker rm clockbox-container || true
 
 # Run the new container
-docker run -d -p 80:8080 --name clockbox-container ${secrets.DOCKERHUB_USERNAME}/clockbox:latest
+sudo docker run -d -p 80:8080 --name clockbox-container ${secrets.DOCKERHUB_USERNAME}/clockbox:latest
