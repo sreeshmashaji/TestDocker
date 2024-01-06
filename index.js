e6745104d4949ede5506ceadba6873ab5849034a
sreeshmashaji <sreeshma3023@gmail.com>
2024-01-06 12:09:33 +0530

    new

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ http.createServer(function (req, res) {
   res.write('Hello World!How Are you.....Hope you are doing well '); //write a response to the client
   res.end(); //end the response
 }).listen(8080,()=>{
-    console.log("Server running on 8080");
+    console.log("Server running on 8080..");
 }); //the server object listens on port 8080
