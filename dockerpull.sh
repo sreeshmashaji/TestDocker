ffc2c108a0dac03f52292fc736bdd141611e0d4c
sreeshmashaji <sreeshma3023@gmail.com>
2024-01-08 11:12:57 +0530

    newOne

--- dockerpull.sh
+++ dockerpull.sh
@@ -11,12 +11,12 @@ DOCKER_PASSWORD="Vishnu123#"
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 echo "Pulling image $IMAGE_NAME:$IMAGE_TAG"
-sudo docker pull "$IMAGE_NAME:$IMAGE_TAG"
+sudo docker pull -a "$DOCKER_USERNAME/$IMAGE_NAME"
 
 
 # Stop and remove any existing container with the same name
 sudo docker rm -f "$CONTAINER_NAME" 2> /dev/null || true
 
 # Run the container, exposing ports (but without volumes)
-sudo docker run -dp 8080:8080 --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" 
+sudo docker run -dp 8080:8080 --name "$CONTAINER_NAME" "$DOCKER_USERNAME/$IMAGE_NAME:$IMAGE_TAG" 
 
