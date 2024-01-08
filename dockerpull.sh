8d7ae8283bce1c12c5f4642392abcf5ea5c56cc0
sreeshmashaji <sreeshma3023@gmail.com>
2024-01-08 10:58:42 +0530

    newOne

--- dockerpull.sh
+++ dockerpull.sh
@@ -5,19 +5,15 @@ IMAGE_NAME="clockbox
 IMAGE_TAG="latest"  # Or specify a specific tag
 CONTAINER_NAME="clock_container"
 PORTS="-p 8080:80"  # Adjust as needed for port mappings
-
 # Pull the latest image if it's not already available
 
-  echo "Pulling image $IMAGE_NAME:$IMAGE_TAG"
-  docker pull "$IMAGE_NAME:$IMAGE_TAG"
+echo "Pulling image $IMAGE_NAME:$IMAGE_TAG"
+sudo docker pull "$IMAGE_NAME:$IMAGE_TAG"
 
 
 # Stop and remove any existing container with the same name
-docker rm -f "$CONTAINER_NAME" 2> /dev/null || true
+sudo docker rm -f "$CONTAINER_NAME" 2> /dev/null || true
 
 # Run the container, exposing ports (but without volumes)
-docker run -d --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" $PORTS
+sudo docker run -d --name "$CONTAINER_NAME" "$IMAGE_NAME:$IMAGE_TAG" $PORTS
 
-echo "Container $CONTAINER_NAME started successfully!"
-
-EOF
